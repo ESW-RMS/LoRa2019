a95a0bbc74d3fd267a5464ba76528b66b07d57b1
Sicheng Zeng <zengcs@stanford.edu>
2019-06-02 12:05:19 -0900

    small

--- device2/read_lora_write_serial.py
+++ device2/read_lora_write_serial.py
@@ -45,6 +45,7 @@ while True:
     # check for packet rx
     packet = rfm9x.receive()
     print("-"),
+
     if packet is not None:
 
         # Display the packet text and rssi
@@ -55,10 +56,10 @@ while True:
         ser.write(packet_text.encode('utf-8').strip())
         print("Sent to serial: " + packet_text)
 
-        # Toggle light
-        subprocess.run(["echo " + str(light_status) + " >/sys/class/leds/led0/brightness"], shell=True)
-        light_status = 1 - light_status
-        
         time.sleep(1)
+        
+    # Toggle light
+    subprocess.run(["echo " + str(light_status) + " >/sys/class/leds/led0/brightness"], shell=True)
+    light_status = 1 - light_status
 
     time.sleep(0.1)
--- device2/start_device2.sh
+++ device2/start_device2.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 ### BEGIN INIT INFO
-# Provides:          start_lora
+# Provides:          start_device2
 # Required-Start:    $all
 # Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:
-# Short-Description: Starts the LoRa python script
+# Short-Description: Starts the device 2 script
 ### END INIT INFO
 
-# /etc/init.d/start_lora
+# /etc/init.d/start_device2
 
 sudo chmod 777 /sys/class/leds/led0/trigger
 sudo chmod 777 /sys/class/leds/led0/brightness
