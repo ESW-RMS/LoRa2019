835a1be3c3b4f323165428d6b43df77c25f9d3e7
Sicheng Zeng <zengcs@stanford.edu>
2019-06-12 20:13:17 -0700

    Update setup_device2.sh

--- device2/setup_device2.sh
+++ device2/setup_device2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash 
 
-# Download Libraries 
+sudo apt-get update
+sudo apt-get upgrade
+
+# Download LoRa Libraries 
+# Reference: https://learn.adafruit.com/lora-and-lorawan-radio-for-raspberry-pi/rfm9x-raspberry-pi-setup
+#sudo pip3 install adafruit-circuitpython-ssd1306
+#sudo pip3 install adafruit-circuitpython-framebuf
+sudo pip3 install adafruit-circuitpython-rfm9x
 
 # Set to run on startup 
+echo "/home/pi/LoRa2019/device2/start_device2.sh" >> ~/.profile
